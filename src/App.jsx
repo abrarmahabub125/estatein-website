5ac196734a80d0760e40a082730377edcc89b3aa
abrarmahabub125 <abrarmahabub125@gmail.com>
2025-12-16 16:30:00 +0600

    First commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,9 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
+import VideoPlayer from "./VideoPlayer";
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div className="my-4 flex items-center gap-10 px-10">
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="flex h-screen w-full items-center justify-center gap-8 bg-[#121212]">
+      <VideoPlayer />
+    </div>
   );
 }
 
